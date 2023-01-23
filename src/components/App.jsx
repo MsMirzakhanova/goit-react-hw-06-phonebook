4058a53e748846548cd543fc92b90a4fb01770e0
MsMirzakhanova <98583249+MsMirzakhanova@users.noreply.github.com>
2023-01-23 14:57:50 +0200

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,14 @@
-// import { useState, useEffect } from 'react';
+
 import {Container} from './App.styled';
 import { PhonebookForm } from './Phonebook/PhonebookForm';
 import { ContactsList } from './Contacts/ContactsList';
 import { Filter } from './Filter/Filter';
-// import toast, { Toaster } from 'react-hot-toast';
 
-// const shortid = require('shortid');
 
 
 export function App () {
 
-// const initialContacts = [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ]
 
-//   const [contacts, setContacts] = useState(initialContacts);
-//   const [filter, setFilter] = useState('');
 //   useEffect(() => {
 //     const contacts = localStorage.getItem('contacts');
 //     if (contacts) {
@@ -31,38 +21,9 @@ export function App () {
 //     }
 //   }, [contacts]);
   
-// const formSubmitHandler = (name, number) => { 
-//     const contact = {
-//       id: shortid.generate(),
-//       name,
-//       number,
-//     };
-// const normalizeName = contact.name.toLowerCase();
-// const isNameInContact = contacts.find(
-//       contact => contact.name.toLowerCase() === normalizeName
-// );
-//     isNameInContact ? toast.success(`${contact.name} is already in contacts`) :
-//       setContacts((contacts) => [contact, ...contacts]);
-//   };
-  
 
-  // const deleteContact = (id) => {
-  //   setContacts(contacts.filter(contact => contact.id !== id),
-  //   )
-  // };
 
-//   const getVisibleContacts = () => {
-//     const normalizedFilter = filter.toLowerCase();
 
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
-
-//   const changeFilter = e => {
-//     setFilter(e.currentTarget.value);
-//   }
-  
 
   
     return (
--- src/components/redux/srore.js
+++ src/components/redux/srore.js
@@ -1,7 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import contactsReducer from "./contacts.slice"
 
-export const store = configureStore({
-    reducer: { contacts: contactsReducer}
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const rootReducer = combineReducers({
+    contacts: contactsReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+const persistor = persistStore(store);
+
+export { store, persistor };
